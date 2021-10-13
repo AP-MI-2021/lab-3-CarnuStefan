4df385dadfef25d4444a1d11e54017479a4d18b4
Carnu Stefan <crn.stefan@gmail.com>
2021-10-13 11:29:37 +0300

    Implementat_problema_3

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ Funcția de calcul: get_longest_alternating_signs(lst: list[int]) -> list[int]
 
 17.Media numerelor nu depășește o valoare citită.
 Funcția de calcul: get_longest_average_below(lst: list[int], average: float) -> list[int]
+
+10.Toate numerele sunt pare.
+  - Funcția de calcul: get_longest_all_even(lst: list[int]) -> list[int]
 '''
 def  get_longest_alternating_signs(lst: list[int]) -> list[int]:
     '''
@@ -30,7 +33,6 @@ def  get_longest_alternating_signs(lst: list[int]) -> list[int]:
                 prev=num
             if all_alternating ==True:
                 if dr-st+1>len(result):result=lst[st:dr+1]
-    print (result)
     return result
 
 def test_get_longest_alternating_signs():
@@ -57,11 +59,33 @@ def get_longest_average_below(lst: list[int], average: float) -> list[int]:
                 average_ok=False
             if average_ok ==True:
                 if dr-st+1>len(result):result=lst[st:dr+1]
-    print (result)
     return result
 
 def test_get_longest_average_below():
     assert get_longest_average_below([7,24,5,90,6,4],5)==[6,4]
+
+def get_longest_all_even(lst: list[int]) -> list[int]:
+    '''
+    Determina cel mai lung subsir in care toate numerele sunt pare
+    :param lst: lista cu numere
+    :return: lista cu cel mai lung sir cu numere pare
+    '''
+    result=[]
+    n=len(lst)
+    for st in range (n):
+        for dr in range (st,n):
+            sum=0
+            nr=0
+            par_ok=True
+            for num in  lst[st:dr+1]:
+                if num%2!=0:
+                    par_ok=False
+            if par_ok ==True:
+                if dr-st+1>len(result):result=lst[st:dr+1]
+    return result
+
+def test_get_longest_all_even():
+    get_longest_all_even([12,4,24,42,0,13,15,12,26,46])
 
 def read_lst():
     lst=[]
@@ -73,8 +97,9 @@ def read_lst():
 
 def showmenu():
     print("1. Citire date")
-    print("2. Proprietatea 1: Numere cu semne alternante ")
-    print("3. Proprietatea 2: Numere cu media mai mica decat valoarea data")
+    print("2. Proprietatea 1: Cel mai lung subsir de numere cu semne alternante ")
+    print("3. Proprietatea 2: Cel mai lung subsir de numere cu media mai mica decat valoarea data")
+    print("4. Proprietatea 3: Cel mai lung subsir de numere pare")
     print("x. Exit")
 
 def main():
@@ -84,15 +109,18 @@ def main():
         if opt=='1':
             lst=read_lst()
         elif opt=='2':
-            get_longest_alternating_signs(lst)
+            print (get_longest_alternating_signs(lst))
         elif opt=='3':
             avrg=float(input('Introduceti valoarea mediei'))
-            get_longest_average_below(lst,avrg)
+            print (get_longest_average_below(lst,avrg))
+        elif opt=='4':
+            print (get_longest_all_even(lst))
         elif opt=='x':
             break
 
 
 if __name__ =='__main__':
+    test_get_longest_all_even()
     test_get_longest_alternating_signs()
     test_get_longest_average_below()
     main()
